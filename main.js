c8f6cb57ce3695ab46c5d0889f3e284d2d57b5e1
Qurat-ul-ain <ainie005@gmail.com>
2024-03-25 11:49:06 -0700

    push my operators to github

---
+++ main.js
@@ -0,0 +1,149 @@
+"use strict";
+// let num1: number = 2;
+// let num2: number = 3;
+// let cart: number = num1 + num2;
+// console.log(cart);
+// subtraction
+// let cart: number = 5;
+// let num3: number = 2;
+// let total: number = cart - num3;
+// console.log(total);
+// multiplication
+// let bags: number = 4;
+// let apples: number = 5;
+// let totalApples: number = bags * apples;
+// console.log(totalApples);
+// division
+// let totalApples: number = 20;
+// let bags: number = 5;
+// let eachBags: number = totalApples / bags;
+// console.log(eachBags);
+// exponentiation
+// let layers: number = 5;
+// let apples: number = 5;
+// let power: number = layers ** 2;
+// console.log(power);
+// modulus
+// let apples: number = 5;
+// let bags: number = 2;
+// let reminder: number = apples % bags;
+// console.log(reminder);
+// unary operators
+// let a = 5;
+// let b = 2;
+// a++;
+// b--;
+// console.log(a);
+// console.log(b);
+// let a: number = 5;
+// let b: number = 2;
+// let c = ++a + a++ + --b + b-- + a + b++ + b;
+// console.log(c);
+// let index = 10;
+// ++index;
+// ++index;
+// ++index;
+// console.log(index);
+// 
+// let index: number = 10;
+// --index;
+// index--;
+// console.log(index);
+// let index = 10;
+// let result = ++index + ++index + 6 + --index - 2;
+// console.log(result);
+// assign operators
+// let c = 10;
+// c += 5;
+// console.log(c);
+// let c = 10;
+// c -= 5;
+// console.log(c);
+// comparision operators
+// let a = 5;
+// let b = 2
+// let isEqual = (a == b);
+// let isNotEqual = (a != b);
+// let isGreaterThan = (a > b);
+// let isLessThan = (a < b);
+// console.log(isEqual);
+// console.log(isGreaterThan);
+// console.log(isNotEqual);
+// console.log(isLessThan);
+// // logical operator
+// // let a = 5;
+// // let b = 2;
+// // let logicalAnd = (a > 0) && (b > 0);
+// // let logicalOr = ( a < 0) || (b > 0);
+// // let logicalNot = !(a > 0);
+// // console.log(logicalAnd);
+// // console.log(logicalOr);
+// // console.log(logicalNot);
+// // && operator(both should bee correct)
+// let goodJob = true;
+// let canCook = false;
+// if(goodJob && canCook) {
+//     console.log("Rishta Pakka!!");
+// }
+// else {
+//     console.log("Rejected");
+// }
+// let myPeercentage = 70;
+// if(myPeercentage >= 80){
+//     console.log("your percentage is : A+")
+// }
+// else if (myPeercentage >= 70) {
+//     console.log("your percentage is : A")
+// }
+// else if (myPeercentage >= 60) {
+//     console.log("your percentage is : B")
+// }
+// else {
+//     console.log("you're failed")
+// }
+let myPercentage = 70;
+if (myPercentage >= 80 && myPercentage <= 100) {
+    console.log("your percentage is : A+");
+}
+else if (myPercentage >= 70 && myPercentage <= 79) {
+    console.log("your percentage is : A");
+}
+else if (myPercentage >= 60 && myPercentage <= 69) {
+    console.log("your percentage is : B");
+}
+else {
+    console.log("you're failed");
+}
+// let a = 5;
+// let b = 8;
+// if(a < b && b < a) {
+//     console.log(a)
+// }
+// else{
+//     console.log(b);
+// }
+// or operator(one should be correct)
+// let handsome = true;
+// let rich = false;
+// if (handsome || rich) {
+//     console.log("rishta pakka")
+// }
+// else {
+//     console.log("rejected")
+// }
+// ternary operator
+// if/else
+// let myAge = 17;
+// if (myAge >= 18) {
+//     console.log("you are allowed");
+// }
+// // if(true) {
+// //     console.log("you are allowed");
+// // }
+// // if(false) {
+// //     console.log("your're allowed")
+// // }
+// // else mn round bracket use ni krte
+// else{
+//     console.log("you're not allowed");
+// }
